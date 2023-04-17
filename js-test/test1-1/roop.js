f895fc8818cf0c89549acd18597c02ca33621edf
NaO3430 <fariytail1113@gmail.com>
2023-04-17 10:49:35 +0900

    コード修正

--- js-test/test1-1/roop.js
+++ js-test/test1-1/roop.js
@@ -1,15 +1,12 @@
-// １〜９９まで繰り返す
+const Fizz = 3;
+const Buzz = 5;
+
 for (let i = 1; i <= 99; i++) {
-  // 条件：変数iが３と５で割り切れる場合
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz" + i);
-  }
-  // 条件分岐：変数iが3で割り切れる場合
-  else if (i % 3 === 0) {
-    console.log("Fizz" + i);
-  }
-  // 条件分岐：変数iが5で割り切れる場合
-  else if (i % 5 === 0) {
-    console.log("Buzz" + i);
+  if (i % Fizz === 0 && i % Buzz === 0) {
+    console.log(`FizzBuzz ${i}`);
+  } else if (i % Fizz === 0) {
+    console.log(`Fizz ${i}`);
+  } else if (i % Buzz === 0) {
+    console.log(`Buzz ${i}`);
   }
 }
