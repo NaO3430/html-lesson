e5b68a29710af23766ee8e523a38bfb48b88af33
NaO3430 <fariytail1113@gmail.com>
2023-04-17 22:12:18 +0900

    定数のrename

--- js-test/test1-1/roop.js
+++ js-test/test1-1/roop.js
@@ -1,12 +1,12 @@
-const Fizz = 3;
-const Buzz = 5;
+const fizz = 3;
+const buzz = 5;
 
 for (let i = 1; i <= 99; i++) {
-  if (i % Fizz === 0 && i % Buzz === 0) {
+  if (i % fizz === 0 && i % buzz === 0) {
     console.log(`FizzBuzz ${i}`);
-  } else if (i % Fizz === 0) {
+  } else if (i % fizz === 0) {
     console.log(`Fizz ${i}`);
-  } else if (i % Buzz === 0) {
+  } else if (i % buzz === 0) {
     console.log(`Buzz ${i}`);
   }
 }
