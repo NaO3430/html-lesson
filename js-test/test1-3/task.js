780f8aa24f04674d4c46141ea470e4eaee6f2c7e
NaO3430 <fariytail1113@gmail.com>
2023-04-24 23:41:07 +0900

    アロー関数に修正

--- js-test/test1-3/task.js
+++ js-test/test1-3/task.js
@@ -2,20 +2,20 @@ const borderLine = '='.repeat(24);
 const taskListTitle = '現在持っているタスク一覧';
 const taskList = ['掃除', '買い物', '散歩'];
 
-function loadTasks() {
+showTaskList = () => {
   console.log(borderLine);
   console.log(taskListTitle);
   console.log(borderLine);
   taskList.forEach((task, index) => {
     console.log(`${index} : ${task}`);
   });
-}
+};
 
-loadTasks();
+showTaskList();
 
 const newTask = prompt('新しいタスクを入力して下さい');
 taskList.push(newTask);
 
-loadTasks();
+showTaskList();
 
 alert('新しいタスクが追加されました');
