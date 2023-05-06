855f2a0dbf4965cd08de46ca11c38a6bfc942e52
NaO3430 <fariytail1113@gmail.com>
2023-05-06 18:30:06 +0900

    コード修正

--- js-test3/js-test3-1/todolist1.js
+++ js-test3/js-test3-1/todolist1.js
@@ -1,28 +1,40 @@
-const todoList = [];
+const tasks = [];
 
-const addTask = () => {
-  const input = document.getElementById('add-task');
-  const comment = input.value;
-  input.value = '';
-
+const createStatusBtn = () => {
   const statusBtn = document.createElement('button');
   statusBtn.textContent = '作業中';
+  return statusBtn;
+};
 
+const createDeleteBtn = () => {
   const deleteBtn = document.createElement('button');
   deleteBtn.textContent = '削除';
+  return deleteBtn;
+};
 
-  todoList.push({ comment: comment });
-
+const createTodoList = (name) => {
   const todoListTable = document.getElementById('todo-list');
   const newRow = todoListTable.insertRow();
 
   const idCell = newRow.insertCell();
-  const commentCell = newRow.insertCell();
+  const taskNameCell = newRow.insertCell();
   const statusCell = newRow.insertCell();
   const deleteCell = newRow.insertCell();
 
-  idCell.textContent = todoList.length;
-  commentCell.textContent = comment;
+  const statusBtn = createStatusBtn();
+  const deleteBtn = createDeleteBtn();
+
+  idCell.textContent = tasks.length;
+  taskNameCell.textContent = name;
   statusCell.appendChild(statusBtn);
   deleteCell.appendChild(deleteBtn);
+};
+
+const addTask = () => {
+  const input = document.getElementById('add-task');
+  const name = input.value;
+  input.value = '';
+
+  tasks.push({ name: name, status: '作業中' });
+  createTodoList(name);
 };
